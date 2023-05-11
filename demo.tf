771e1fcc3cd71acc0dbbb3e901a17e477a058bb8
mstar303 <91958773+mstar303@users.noreply.github.com>
2023-05-11 14:54:19 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAQJPGYTKW7BMQSHGE"
-  secret_key = "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"
+  access_key = "E"
+  secret_key = "4xv32BQFqq"
 }
 
 resource "aws_instance" "myec2" {
