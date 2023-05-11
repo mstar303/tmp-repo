b6b358567a8459e3c164276bb1c468a4c173aeac
mstar303 <91958773+mstar303@users.noreply.github.com>
2023-05-11 14:28:23 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,20 +1,11 @@
-locals {
-  arr = ["host1", "host2", "host3"]
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAQJPGYTKW7BMQSHGE"
+  secret_key = "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"
 }
 
-
-locals {
-  arr2 = ["host1", "host2", "host3"]
-}
-
-locals {
-  arr3 = ["host1", "host2", "host3"]
-}
-
-output "test" {
-  value = local.arr
-}
-
-output "tes2" {
-  value = local.arr3
+resource "aws_instance" "myec2" {
+  ami           = "ami-082b5a644766e0e6f"
+  instance_type = "t2.micro"
+  
 }
